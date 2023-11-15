985e19d7127d069aeba2a020019a7cf703eee368
qingzeqi <qingzeqi@gmail.com>
2023-11-16 02:30:57 +0800

    Delete lcd2_xiaochen directory

--- lcd2_xiaochen/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo echo 'hdmi_force_hotplug=1' >> /boot/config.txt
-
-cd /home/pi/.config/
-mkdir /home/pi/.config/autostart
-cd /home/pi/.config/autostart
-cp /home/pi/lcd2_tytion/start.desktop /home/pi/.config/autostart/
-echo 'install ok!'
--- lcd2_xiaochen/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/pi/lcd2_tytion/examples \
-python lcd2-ty.py \
